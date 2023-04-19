b0da6a3f214a055d0f7e6ae4de3c897b1da2bf58
Mikael Picard <mpicard@aubay.com>
2023-04-19 14:19:20 +0200

    feat - Suppression du code lié à JUnit4

--- html-plugin/src/test/java/fr/greencodeinitiative/html/checks/AvoidAutoplayCheckTest.java
+++ html-plugin/src/test/java/fr/greencodeinitiative/html/checks/AvoidAutoplayCheckTest.java
@@ -2,42 +2,39 @@ package fr.greencodeinitiative.html.checks;
 
 import java.io.File;
 
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.migrationsupport.rules.EnableRuleMigrationSupport;
 import org.sonar.plugins.html.visitor.HtmlSourceCode;
 
-@EnableRuleMigrationSupport
 public class AvoidAutoplayCheckTest {
     
-    @Rule
-    public CheckMessagesVerifierRule checkMessagesVerifier = new CheckMessagesVerifierRule();
-
     @Test
     public void should_detect_on_html_documents() {
         HtmlSourceCode sourceCode = TestHelper.scan(new File("src/test/resources/checks/AvoidAutoplayCheck/document.html"), new AvoidAutoplayCheck());
         
-        checkMessagesVerifier.verify(sourceCode.getIssues())
+        CheckMessagesVerifier.verify(sourceCode.getIssues())
             .next().atLine(7).withMessage("Remove autoplay attribute to this audio")
-            .next().atLine(9).withMessage("Remove autoplay attribute to this video");
+            .next().atLine(9).withMessage("Remove autoplay attribute to this video")
+            .noMore();
     }
     
     @Test
     public void should_detect_on_php_documents() {
         HtmlSourceCode sourceCode = TestHelper.scan(new File("src/test/resources/checks/AvoidAutoplayCheck/document.php"), new AvoidAutoplayCheck());
         
-        checkMessagesVerifier.verify(sourceCode.getIssues())
+        CheckMessagesVerifier.verify(sourceCode.getIssues())
             .next().atLine(6).withMessage("Remove autoplay attribute to this audio")
-            .next().atLine(9).withMessage("Remove autoplay attribute to this video");
+            .next().atLine(9).withMessage("Remove autoplay attribute to this video")
+            .noMore();
     }
     
     @Test
     public void should_detect_on_jsp_documents() {
         HtmlSourceCode sourceCode = TestHelper.scan(new File("src/test/resources/checks/AvoidAutoplayCheck/document.jsp"), new AvoidAutoplayCheck());
         
-        checkMessagesVerifier.verify(sourceCode.getIssues())
+        CheckMessagesVerifier.verify(sourceCode.getIssues())
             .next().atLine(7).withMessage("Remove autoplay attribute to this audio")
-            .next().atLine(9).withMessage("Remove autoplay attribute to this video");
+            .next().atLine(9).withMessage("Remove autoplay attribute to this video")
+            .noMore();
     }
     
 }
--- html-plugin/src/test/java/fr/greencodeinitiative/html/checks/CheckMessagesVerifier.java
+++ html-plugin/src/test/java/fr/greencodeinitiative/html/checks/CheckMessagesVerifier.java
@@ -15,90 +15,90 @@ public class CheckMessagesVerifier {
 
     public static CheckMessagesVerifier verify(Collection<HtmlIssue> messages) {
         return new CheckMessagesVerifier(messages);
-      }
+    }
 
-      private final Iterator<HtmlIssue> iterator;
-      private HtmlIssue current;
+    private final Iterator<HtmlIssue> iterator;
+    private HtmlIssue current;
 
-      private static final Comparator<HtmlIssue> ORDERING = (left, right) -> {
+    private static final Comparator<HtmlIssue> ORDERING = (left, right) -> {
         if (Objects.equals(left.line(), right.line())) {
-          return left.message().compareTo(right.message());
+            return left.message().compareTo(right.message());
         } else if (left.line() == null) {
-          return -1;
+            return -1;
         } else if (right.line() == null) {
-          return 1;
+            return 1;
         } else {
-          return left.line().compareTo(right.line());
+            return left.line().compareTo(right.line());
         }
-      };
+    };
 
-      private CheckMessagesVerifier(Collection<HtmlIssue> messages) {
+    private CheckMessagesVerifier(Collection<HtmlIssue> messages) {
         ArrayList<HtmlIssue> messagesList = new ArrayList<>(messages);
         messagesList.sort(ORDERING);
         iterator = messagesList.iterator();
-      }
+    }
 
-      public CheckMessagesVerifier next() {
+    public CheckMessagesVerifier next() {
         if (!iterator.hasNext()) {
-          throw new AssertionError("\nExpected violation");
+            throw new AssertionError("\nExpected violation");
         }
         current = iterator.next();
         return this;
-      }
+    }
 
-      public void noMore() {
+    public void noMore() {
         if (iterator.hasNext()) {
-          HtmlIssue next = iterator.next();
-          throw new AssertionError("\nNo more violations expected\ngot: at line " + next.line());
+            HtmlIssue next = iterator.next();
+            throw new AssertionError("\nNo more violations expected\ngot: at line " + next.line());
         }
-      }
+    }
 
-      private void checkStateOfCurrent() {
+    private void checkStateOfCurrent() {
         if (current == null) {
-          throw new IllegalStateException("Prior to this method you should call next()");
+            throw new IllegalStateException("Prior to this method you should call next()");
         }
-      }
+    }
 
-      public CheckMessagesVerifier atLine(@Nullable Integer expectedLine) {
+    public CheckMessagesVerifier atLine(@Nullable Integer expectedLine) {
         checkStateOfCurrent();
         if (!Objects.equals(expectedLine, current.line())) {
-          throw new AssertionError("\nExpected: " + expectedLine + "\ngot: " + current.line());
+            throw new AssertionError("\nExpected: " + expectedLine + "\ngot: " + current.line());
         }
         return this;
-      }
+    }
 
-      public CheckMessagesVerifier atLocation(int startLine, int startColumn, int endLine, int endColumn) {
+    public CheckMessagesVerifier atLocation(int startLine, int startColumn, int endLine, int endColumn) {
         checkStateOfCurrent();
         PreciseHtmlIssue preciseHtmlIssue = (PreciseHtmlIssue) current;
         if (!Objects.equals(startLine, current.line())) {
-          throw new AssertionError("\nExpected: " + startLine + "\ngot: " + current.line());
+            throw new AssertionError("\nExpected: " + startLine + "\ngot: " + current.line());
         }
         if (!Objects.equals(startColumn, preciseHtmlIssue.startColumn())) {
-          throw new AssertionError("\nExpected: " + startColumn + "\ngot: " + preciseHtmlIssue.startColumn());
+            throw new AssertionError("\nExpected: " + startColumn + "\ngot: " + preciseHtmlIssue.startColumn());
         }
         if (!Objects.equals(endLine, preciseHtmlIssue.endLine())) {
-          throw new AssertionError("\nExpected: " + endLine + "\ngot: " + preciseHtmlIssue.endLine());
+            throw new AssertionError("\nExpected: " + endLine + "\ngot: " + preciseHtmlIssue.endLine());
         }
         if (!Objects.equals(endColumn, preciseHtmlIssue.endColumn())) {
-          throw new AssertionError("\nExpected: " + endColumn + "\ngot: " + preciseHtmlIssue.endColumn());
+            throw new AssertionError("\nExpected: " + endColumn + "\ngot: " + preciseHtmlIssue.endColumn());
         }
         return this;
-      }
+    }
 
-      public CheckMessagesVerifier withMessage(String expectedMessage) {
+    public CheckMessagesVerifier withMessage(String expectedMessage) {
         checkStateOfCurrent();
         String actual = current.message();
         if (!actual.equals(expectedMessage)) {
-          throw new AssertionError("\nExpected: \"" + expectedMessage + "\"\ngot: \"" + actual + "\"");
+            throw new AssertionError("\nExpected: \"" + expectedMessage + "\"\ngot: \"" + actual + "\"");
         }
         return this;
-      }
+    }
 
-      public CheckMessagesVerifier withCost(@Nullable Double expectedCost) {
+    public CheckMessagesVerifier withCost(@Nullable Double expectedCost) {
         checkStateOfCurrent();
         if (!Objects.equals(expectedCost, current.cost())) {
-          throw new AssertionError("\nExpected: " + expectedCost + "\ngot: " + current.cost());
+            throw new AssertionError("\nExpected: " + expectedCost + "\ngot: " + current.cost());
         }
         return this;
-      }
+    }
 }
